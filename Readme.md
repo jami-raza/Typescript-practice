767f28d2b95c541e8453ebfdc9ec01fb15dfbe0b
jami-raza <57630996+jami-raza@users.noreply.github.com>
2020-08-07 16:04:51 +0500

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,4 @@
+## Local installation
+
+First create project with npm init
+npm install typescript@beta
---
+++ scr/demo1.ts
@@ -0,0 +1,11 @@
+document.write("Hello World")
+
+function Evenorodd(num:number){
+    if (num % 2 === 0)
+    return("number is even")
+    else 
+    return("number is odd")
+
+}
+console.log('This ',Evenorodd(10));
+console.log('This ',Evenorodd(5));
